582478280f6b1d50758966e77fa89bcee4b73b4a
Niko <nicosejohnsoriano@gmail.com>
2023-06-23 23:25:38 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import About from './pages/About';
+import './App.scss';
 
 function App() {
   return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,41 +1,74 @@
 import { Link } from 'react-router-dom'
+import { BsGithub, BsQuestionCircle, BsCodeSlash, BsChatLeftHeart } from 'react-icons/bs';
 import Navbar from '../components/Navbar'
 import todo from '../images/Checklist-rafiki.svg';
 import '../styles/Home.scss'
 
 export default function Home() {
-    return (
-        <>
-          <Navbar/>
-          <div className="home-body">
-            <section className="container-1">
-              <div className="home-info">
-              <h1>Elevate your life through <span>Gamification!</span>
-              </h1>
-              <p>An innovative game concept platform for to-do list tasks! 
-              </p>
-              <div className="home-btn">
-                <button className="primary-btn-1">
-                  <Link to="/Signup" className="btn-1">Try Now</Link>
-                  </button>
-                <button className="primary-btn-2">
-                  <Link to="/About" className="btn-2">Open Source</Link>
-                  </button>
-              </div>
-              </div>
-              <div className="home-image">
-                <img className="contain-image" src={todo} alt="todo"/>
-              </div>
-            </section>
-
-            <section className="container-2">
-              <div className="box-1"></div>
-              <div className="box-2"></div>
-              <div className="box-3"></div>
-            </section>
+  return (
+    <>
+      <Navbar />
+      <div className="home-body">
+        <section className="container-1">
+          <div className="home-info">
+            <h1>Elevate your life through <span>Gamification!</span>
+            </h1>
+            <p>An innovative game concept platform for to-do list tasks!
+            </p>
+            <div className="home-btn">
+              <button className="primary-btn-1">
+                <Link to="/Signup" className="btn-1">Try Now</Link>
+              </button>
+              <button className="primary-btn-2">
+                <Link to="/About" className="btn-2">Open Source <BsGithub className="icons-1" /></Link>
+              </button>
+            </div>
           </div>
+          <div className="home-image">
+            <img className="contain-image" src={todo} alt="todo" />
+          </div>
+        </section>
 
-         
-        </>
-    )
+        <section className="container-2">
+          <div className="col">
+            <div className="box">
+              <BsQuestionCircle className="box-icon" />
+              <h1>Focus Zone Platform</h1>
+              <p>Know more about Focus Zone!</p>
+              <div className="box-button">
+                <Link to="/About" className="box-btn-1">About</Link>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="box">
+              <BsChatLeftHeart className="box-icon" />
+              <h1>Message me</h1>
+              <p>Reach me out in my available platforms!</p>
+              <div className="box-button">
+                <Link to="/" className="box-btn-1">Facebook Page</Link>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="box">
+              <BsCodeSlash className="box-icon" />
+              <h1>Open Source</h1>
+              <p>Contribute, report bugs, and request features!</p>
+              <div className="box-button">
+                <Link to="/" className="box-btn-1">Github Repo</Link>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <div className="end-1">
+          <div></div>
+        </div>
+
+
+
+      </div>
+    </>
+  )
 }
