34bf6b1c8cc97b997f66125a49fc44c11035b4bf
Niko <nicosejohnsoriano@gmail.com>
2023-07-05 04:41:42 +0800

    firebase added

---
+++ src/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDhM4AsMhW6qRXxwj5SuPM3Axt22qmhDzw",
+    authDomain: "focus-zone-6abdb.firebaseapp.com",
+    projectId: "focus-zone-6abdb",
+    storageBucket: "focus-zone-6abdb.appspot.com",
+    messagingSenderId: "399766586547",
+    appId: "1:399766586547:web:00bfe8fae37d139a0e9cc6",
+    measurementId: "G-32JL8G5L3G"
+};
+
+const app = initializeApp(firebaseConfig);
