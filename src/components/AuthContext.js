d501f60df9c29ae4c523becd8ddb672cea121bc6
Niko <nicosejohnsoriano@gmail.com>
2023-07-26 08:06:31 +0800

    google sign-in auth

---
+++ src/components/AuthContext.js
@@ -0,0 +1,22 @@
+import { useContext, createContext } from 'react';
+import { GoogleAuthProvider, signInWithPopup, signInWithRedirect, signOut, onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebase-config';
+
+const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+    const googleSignIn = () => {
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider);
+    }
+
+    return (
+        <AuthContext.Provider value={{ googleSignIn }}>
+          {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const UserAuth = () => {
+    return useContext(AuthContext);
+};
